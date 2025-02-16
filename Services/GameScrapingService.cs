e64f5b640e68be0d8de8c7f2bcdcbefcb97f52be
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-02-16 19:56:44 +0200

    added neccesary variables and methods

--- Services/GameScrapingService.cs
+++ Services/GameScrapingService.cs
@@ -1,7 +1,40 @@
 using HtmlAgilityPack;
+using nba_results_notifier.Interfaces;
+using nba_results_notifier.Models;
+
 namespace nba_results_notifier.Services;
 
-public class ScrapingService
+public class GameScrapingService : IGameScraper
 {
+    private const string URL = "https://www.basketball-reference.com/boxscores/";
+    private readonly HtmlWeb _web;
+    private readonly HtmlDocument _document;
+    List<Game>? games = new();
     
+    public GameScrapingService() {
+        _web = new HtmlWeb();
+        _document = _web.Load(URL);
+    }
+    
+    public List<Game>? GetGames()
+    {
+        HtmlNodeCollection nodes = _document.DocumentNode.SelectNodes("//div/div[contains(@class, 'game_summary')]");
+
+        foreach (HtmlNode node in nodes)
+        {
+                ExtractGameInfo(node);
+        }
+
+        return games;
+    }
+
+    private void ExtractGameInfo(HtmlNode node)
+    {
+        
+    }
+
+    private void ExtractTeamInfo(HtmlNode node)
+    {
+        
+    }
 }
