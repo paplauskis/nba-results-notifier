eac1cd7ddd9957c794fe55a3034cc03de7a190bc
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-02-16 21:34:20 +0200

    add properties

--- Models/Team.cs
+++ Models/Team.cs
@@ -2,5 +2,15 @@ namespace nba_results_notifier.Models;
 
 public class Team
 {
+    public string Name { get; set; }
+
+    public short TotalPoints { get; set; }
     
+    public short FirstQuarterPoints { get; set; }
+    
+    public short SecondQuarterPoints { get; set; }
+    
+    public short ThirdQuarterPoints { get; set; }
+    
+    public short FourthQuarterPoints { get; set; }
 }
