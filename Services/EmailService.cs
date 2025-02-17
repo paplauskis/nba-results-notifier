0737027c5ca7012b01aa6cada6bd1f8984da9b93
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-02-17 12:02:08 +0200

    added method to configure email message

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,6 +1,37 @@
+using System.Net;
+using System.Net.Mail;
+using nba_results_notifier.Interfaces;
+
 namespace nba_results_notifier.Services;
 
-public class EmailService
+public class EmailService : ISendable
 {
-    
+    public Task Send(string subject, string body)
+    {
+        MailMessage message = ConfigureMessage(subject, body);
+        
+        SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+        
+        return smtpClient.SendMailAsync(message);
+    }
+
+    private static MailMessage ConfigureMessage(string subject, string body)
+    {
+        MailMessage message = new MailMessage(
+            Environment.GetEnvironmentVariable("SENDER_EMAIL"), 
+            Environment.GetEnvironmentVariable("RECEIVER_EMAIL"));
+        
+        message.From = new MailAddress(Environment.GetEnvironmentVariable("SENDER_EMAIL"));
+        message.To.Add(new MailAddress(Environment.GetEnvironmentVariable("RECEIVER_EMAIL")));
+        message.Subject = subject;
+        message.IsBodyHtml = true;
+        message.Body = body;
+        
+        return message;
+    }
+
+    private static SmtpClient ConfigureSmtpClient()
+    {
+        throw new NotImplementedException();
+    }
 }
