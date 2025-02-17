a6c897b8277b240c1f856d79dab997c78a6190b1
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-02-17 14:33:57 +0200

    combined main logic to send message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using nba_results_notifier.Interfaces;
+using nba_results_notifier.Services;
 
-Console.WriteLine("Hello, World!");
+var games = new GameScrapingService().GetGames();
+
+var razorService = new RazorTemplateService();
+string emailBody = await razorService.RenderAsync("GameResultTables", games);
+ISendable emailService = new EmailService();
+
+await emailService.Send(
+    $"Last Night's NBA Game Results ({DateTime.Now.AddDays(-1):yyyy-MM-dd})",
+    emailBody
+);
